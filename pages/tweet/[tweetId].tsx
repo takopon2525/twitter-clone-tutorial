fbf81bacd6df31f7ef0ece39b95684454b0ff037
takopon <doluche.avalanche@gmail.com>
2022-09-06 17:26:30 +0900

    lesson8

--- pages/tweet/[tweetId].tsx
+++ pages/tweet/[tweetId].tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 import Head from "next/head";
@@ -12,16 +12,32 @@ import {
   AiOutlineUpload,
   AiOutlineArrowLeft,
 } from "react-icons/ai";
+import TimeAgo from "react-timeago";
+import japanStrings from "react-timeago/lib/language-strings/ja";
+import buildFormatter from "react-timeago/lib/formatters/buildFormatter";
+import toast from "react-hot-toast";
 import Link from "next/link";
 
 function Tweet() {
+  const formatter = buildFormatter(japanStrings);
   const { data: sesson } = useSession();
   const router = useRouter();
-  console.log(router.query)
+  const [comment, setComment] = useState<string>("");
+  console.log(router.query);
   const { loading, error, data } = useQuery(GET_TWEET_BY_ID, {
     variables: { id: router.query.tweetId },
   });
   const tweet = data?.getTweet;
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const notification = toast.loading("ツイートを投稿しています...");
+    try {
+    } catch (error) {
+      toast.error("コメントの投稿に失敗しました。", {
+        id: notification,
+      });
+    }
+  };
   if (!tweet || loading)
     return (
       <div
@@ -46,6 +62,101 @@ function Tweet() {
               </div>
             </Link>
             <h1 className="p-3 text-xl font-bold">ツイート</h1>
+          </div>
+          <div className="flex flex-col space-x-3 border-y border-gray-100 p-5">
+            <div className="flex space-x-3">
+              <img
+                className="h-13 w-13 rounded-full object-cover"
+                src={tweet.image || ""}
+                alt=""
+              ></img>
+              <div>
+                <div className="flex items-center space-x-3">
+                  <p className="font-bold">{tweet.username}</p>
+                  <TimeAgo
+                    className="text-sm text-gray-500"
+                    date={tweet.created_at}
+                    formatter={formatter}
+                  />
+                </div>
+                <p className="pt-1">{tweet.text}</p>
+              </div>
+            </div>
+            <div className="mt-5 flex justify-between">
+              <div className="flex cursor-pointer items-center space-x-3 text-gray-400">
+                <div className="p-3 hover:bg-slate-200 hover:rounded-full">
+                  <AiOutlineMessage className="h-5 w-5" />
+                </div>
+                <p>{tweet.commentList.length}</p>
+              </div>
+              <div className="flex cursor-pointer items-center space-x-3 text-gray-400">
+                <div className="p-3 hover:bg-slate-200 hover:rounded-full">
+                  <AiOutlineRetweet className="h-5 w-5" />
+                </div>
+              </div>
+              <div className="flex cursor-pointer items-center space-x-3 text-gray-400">
+                <div className="p-3 hover:bg-slate-200 hover:rounded-full">
+                  <AiOutlineHeart className="h-5 w-5" />
+                </div>
+                <p>0</p>
+              </div>
+              <div className="flex cursor-pointer items-center space-x-3 text-gray-400">
+                <div className="p-3 hover:bg-slate-200 hover:rounded-full">
+                  <AiOutlineUpload className="h-5 w-5" />
+                </div>
+              </div>
+            </div>
+            {/* 新規コメントの追加 */}
+
+            <form onSubmit={handleSubmit} className="mt-3 flex space-x-3">
+              <input
+                value={comment}
+                onChange={(e) => setComment(e.target.value)}
+                className="flex-1 rounded-lg bg-gray-100 p-2 outline-none"
+                type="text"
+                placeholder="返信をツイート"
+              />
+              <button
+                disabled={!comment}
+                type="submit"
+                className="rounded-full bg-twitter text-white px-5 disabled:text-gray-200 hover:bg-sky-600"
+              >
+                返信
+              </button>
+            </form>
+
+            {/* 既にあるコメントを追加 */}
+            {tweet.commentList.length > 0 && (
+              <div className="my-2 mt-5 max-h-52 space-y-5 overflow-y-auto border-t border-gray-100 p-5">
+                {tweet.commentList?.map((comment: any, index: number) => (
+                  <div key={comment.id} className="relative flex space-x-2">
+                    {/* 最後のhr表示は無しにする */}
+                    <hr
+                      className={
+                        tweet.commentList.length - 1 === index
+                          ? "hidden"
+                          : "absolute left-5 top-10 h-8 border-x"
+                      }
+                    />
+                    <img
+                      src={comment.image}
+                      className="mt-2 h-7 w-7 rounded-full object-cover"
+                    />
+                    <div>
+                      <div className="flex items-center space-x-3">
+                        <p className="mr-1 font-bold">{comment.username}</p>
+                        <TimeAgo
+                          className="text-sm text-gray-500"
+                          date={comment.created_at}
+                          formatter={formatter}
+                        />
+                      </div>
+                      <p>{comment.text}</p>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            )}
           </div>
         </div>
       )}
