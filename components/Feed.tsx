f5ddddb31d362846face426378b91bbb66067bf1
takopon <doluche.avalanche@gmail.com>
2022-09-06 15:14:18 +0900

    lesson6

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,16 +1,21 @@
 import { useQuery } from "@apollo/client";
 import React from "react";
 import { GET_ALL_TWEETS } from "../graphql/queris";
+import Tweet from "./Tweet";
 
 function Feed() {
   const { data, loading, error } = useQuery(GET_ALL_TWEETS);
-  console.log(data);
+  const tweets: Tweet[] = data?.getTweetList;
   return (
     <div>
       {/* tweet post box */}
       <div>This is tweet post component</div>
       {/* feed component */}
-      <div>This is tweet component</div>
+      <div>
+        {tweets?.map((tweet) => (
+          <Tweet tweet={tweet} key={tweet.id} />
+        ))}
+      </div>
     </div>
   );
 }
