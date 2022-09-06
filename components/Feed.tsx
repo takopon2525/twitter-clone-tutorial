87935affbc1574381909e4f35767241148ecd157
takopon <doluche.avalanche@gmail.com>
2022-09-06 15:21:50 +0900

    lesson6

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -2,10 +2,21 @@ import { useQuery } from "@apollo/client";
 import React from "react";
 import { GET_ALL_TWEETS } from "../graphql/queris";
 import Tweet from "./Tweet";
+import { Ring } from "@uiball/loaders";
 
 function Feed() {
   const { data, loading, error } = useQuery(GET_ALL_TWEETS);
   const tweets: Tweet[] = data?.getTweetList;
+  if (loading)
+    return (
+      <div
+        className="col-span-7 lg:col-span-5 flex w-full items-center justify-center p-10 text-xl"
+        aria-live="polite"
+        aria-busy={!tweets}
+      >
+        <Ring size={50} color="#3899e8" />
+      </div>
+    );
   return (
     <div>
       {/* tweet post box */}
