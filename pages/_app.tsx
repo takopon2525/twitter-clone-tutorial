25580befc149e4533bb43689d3cd7e1dfe6ca9eb
takopon <doluche.avalanche@gmail.com>
2022-09-06 09:29:19 +0900

    lesson5

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,18 +2,22 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
 import Sidebar from "../components/Sidebar";
+import client from "../apollo-client";
+import { ApolloProvider } from "@apollo/client";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
-    <SessionProvider session={session}>
-      <div className="mx-auto max-h-screen lg:max-w-6xl">
-        <main className="grid grid-cols-9">
-          <Sidebar />
-          <Component {...pageProps} />
-          {/* Widget */}
-        </main>
-      </div>
-    </SessionProvider>
+    <ApolloProvider client={client}>
+      <SessionProvider session={session}>
+        <div className="mx-auto max-h-screen lg:max-w-6xl">
+          <main className="grid grid-cols-9">
+            <Sidebar />
+            <Component {...pageProps} />
+            {/* Widget */}
+          </main>
+        </div>
+      </SessionProvider>
+    </ApolloProvider>
   );
 }
 
