1d2d19a044d3cf101caad17b9e92476641d7a8fb
takopon <doluche.avalanche@gmail.com>
2022-09-06 09:43:48 +0900

    ベースとなるコンポーネントの追加

---
+++ components/Feed.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Feed() {
+  return (
+    <div>
+      {/* tweet post box */}
+      <div>This is tweet post component</div>
+      {/* feed component */}
+      <div>This is tweet component</div>
+    </div>
+  );
+}
+
+export default Feed;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,16 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
+import Feed from "../components/Feed";
 
 const Home: NextPage = () => {
   return (
     <div>
       <Head>
-        <title>Create Next App</title>
+        <title>Twitter Clone Tutorial Apps</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Feed />
     </div>
   );
 };
